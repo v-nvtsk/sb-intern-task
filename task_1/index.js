02a2934083e5c3fac040ca937b671962f9b33ebc
v-nvtsk <v@nvtsk.ru>
2024-04-20 22:13:02 +0300

    feat(task1): add decode function

--- task_1/index.js
+++ task_1/index.js
@@ -1,8 +1,28 @@
-import {encoded, translations} from './data.js'
+import { encoded, translations } from './data.js';
 
-console.log("Let's rock")
-console.log(encoded, translations)
+// console.log("Let's rock");
+// console.log(encoded, translations);
 
+function endsWithId(str) {
+  return str.endsWith('Id') || str.endsWith('id');
+}
 
+function decode(data, translations) {
+  const list = data.map((item) => {
+    return Object.keys(item).reduce((acc, key) => {
+      let field = item[key];
+      if (endsWithId(key) && key !== 'groupId' && field !== null) {
+        if (translations.hasOwnProperty(field)) field = translations[field];
+      }
+      acc[key] = field;
+      return acc;
+    }, {});
+  });
 
-// console.log(decoded)
+  return list;
+}
+
+const decoded = decode(encoded, translations);
+
+console.log(decoded);
+
