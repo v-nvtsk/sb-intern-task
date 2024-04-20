673e4c0c95db600d4dd497dd75cd65e0b0cb3341
v-nvtsk <v@nvtsk.ru>
2024-04-20 22:13:23 +0300

    feat(task1): add getUniqueIds function

--- task_1/index.js
+++ task_1/index.js
@@ -26,3 +26,28 @@ const decoded = decode(encoded, translations);
 
 console.log(decoded);
 
+function getUniqueIds(data) {
+  const translationsIds = Object.keys(translations);
+  const missedInTranslations = {};
+  const ids = {};
+  data.forEach((item) => {
+    Object.keys(item).forEach((key) => {
+      if (endsWithId(key) && key !== 'groupId' && item[key] !== null) {
+        ids[item[key]] = ids[item[key]] + 1 || 1;
+        if (!translationsIds.includes(item[key]))
+          missedInTranslations[item[key]] = item[key];
+      }
+    });
+  });
+
+  // Unique Ids were used only once
+  const uniquelyUsedIds = Object.keys(ids).filter((key) => ids[key] === 1);
+
+  return {
+    ids: Object.keys(ids),
+    uniquelyUsedIds: uniquelyUsedIds,
+    missedInTranslations: Object.keys(missedInTranslations),
+  };
+}
+
+console.log(getUniqueIds(encoded));
